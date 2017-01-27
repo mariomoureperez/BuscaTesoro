70f9f2ce8763f12736155c140bfb1457afca39ea
mario <mariomoureperez@gmail.com>
2017-01-27 18:51:08 +0100

    conseguido calcular distancia de mi posicion a punto

--- main/java/com/mario/localizacion/MapsActivity.java
+++ main/java/com/mario/localizacion/MapsActivity.java
@@ -1,18 +1,33 @@
 package com.mario.localizacion;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Locale;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, LocationListener {
+    public static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
+   public static double lat1,lng1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +52,102 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
+        //activar la escucha para detectar si pulsamos la pantalla y que nos salan las coordenadas
+        mMap.setOnMapClickListener(this);
+        // Add a marker in vigo and move the camera
         LatLng telePizza = new LatLng(42.236954, -8.712717);
-        mMap.addMarker(new MarkerOptions().position(telePizza).title("Marca en TelePizza vigo"));
+
+        Marker marcaT = mMap.addMarker(new MarkerOptions().position(telePizza).title("TelePizza").snippet("Marca TelePizza vigo"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(telePizza));
+        // marcaT.setVisible(false);
+
+        // Controles UI
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Mostrar diálogo explicativo
+            } else {
+                // Solicitar permiso
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            }
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            // ¿Permisos asignados?
+            if (permissions.length > 0 &&
+                    permissions[0].equals(android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Error de permisos", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+     calcularDistancia();
+        
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        // Este mŽtodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+        // debido a la detecci—n de un cambio de ubicacion
+        lat1=loc.getLatitude();
+        lng1=loc.getLongitude();
+        String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+
+    }
+
+    public void calcularDistancia() {
+
+        double earthRadius = 6372.795477598;
+        double lat2=42.236954;
+        double lng2=-8.712717;
+
+        double dLat = Math.toRadians(lat1-lat2);
+        double dLng = Math.toRadians(lng1-lng2);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(lat1)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+
+        String distancia=String.valueOf(dist);
+        Toast.makeText(this, distancia, Toast.LENGTH_LONG).show();
+
+    }
+
+
+
 }
